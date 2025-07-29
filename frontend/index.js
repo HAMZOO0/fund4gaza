4b75f3c2013d6b4c3d89fa82f42600e4488f8b41
Hamza <hamzakhan174722@gmail.com>
2025-07-29 11:02:07 +0500

    test case

--- frontend/index.js
+++ frontend/index.js
@@ -86,6 +86,10 @@ async function getBalance() {
       alert(`Contract Balance: ${ethers.utils.formatEther(string_balance)} ETH`);
 
       console.log(ethers.utils.formatEther(string_balance));
+
+      // Test basic call
+      const testCall = await contract.provider.getCode(ContractAddress);
+      console.log("Contract code at address:", testCall);
    }
 }
 
